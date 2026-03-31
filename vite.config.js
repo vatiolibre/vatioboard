dbe4a1fbb1ac3f461dd8b24dc00900d473df5fc7
oscarperezt <oscar@coltrack.com>
2026-03-31 00:19:28 -0400

    Add BFF proof of concept

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ export default defineConfig({
   base: "/",
   server: {
     host: true,
-    allowedHosts: ["debug.vatiolibre.com", "vatioboard.com", ".vatiolibre.com"],
+    port: 5174,
+    strictPort: true,
+    allowedHosts: ["vatioboard.com", ".vatioboard.com", ".vatiolibre.com"],
   },
   build: {
     rollupOptions: {
@@ -16,6 +18,7 @@ export default defineConfig({
         replay: resolve(__dirname, "replay.html"),
         gpsRate: resolve(__dirname, "gps-rate.html"),
         accel: resolve(__dirname, "accel.html"),
+        login: resolve(__dirname, "login.html"),
       },
     },
   },
